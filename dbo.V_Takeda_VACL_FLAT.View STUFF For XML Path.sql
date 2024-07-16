967cd58463f9d26bcefcfd8d1208529e61d40497
Susanne J <74077559+susannejoest@users.noreply.github.com>
2024-07-16 04:19:20 +0200

    Update and rename dbo.V_Takeda_VACL_FLAT.View.sql to dbo.V_Takeda_VACL_FLAT.View STUFF For XML Path.sql

---
+++ dbo.V_Takeda_VACL_FLAT.View STUFF For XML Path.sql
@@ -0,0 +1,74 @@
+﻿USE [DAQ-1445_Contiki_App_DESQL016_Divestment]
+GO
+/****** Object:  View [dbo].[V_Takeda_VACL_FLAT]    Script Date: 24 Jun 2024 11:12:11 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE view [dbo].[V_Takeda_VACL_FLAT]
+/* could also use string_agg(concat(name,', '),'') */
+as 
+
+SELECT 
+		OBJECTID
+
+	/* group */
+		, Replace(STUFF(
+		(SELECT ';' + s.USERGROUP
+		FROM V_Takeda_VACL_Contract_ReadPrivilege s
+		WHERE s.objectid =p1.objectid
+		FOR XML PATH('')),1,1,''),'&amp;','&') 
+	AS ACL_AllPermissions_GroupList
+
+	/* user */
+		, Replace(STUFF(
+		(SELECT ';' + s.[DISPLAYNAME]
+		FROM V_Takeda_VACL_Contract_ReadPrivilege s
+		WHERE s.objectid =p1.objectid
+		FOR XML PATH('')),1,1,''),'&amp;','&') 
+	AS ACL_AllPermissions_UserList
+
+	/* group and user */
+		, 'GROUPS: ' 
+		+ Replace(STUFF(
+		(SELECT ';' + s.USERGROUP
+		FROM V_Takeda_VACL_Contract_ReadPrivilege s
+		WHERE s.objectid =p1.objectid
+		FOR XML PATH('')),1,1,''),'&amp;','&') 
+		+ ', USERS: ' 
+		+ replace(STUFF(
+		(SELECT ';' + s.[DISPLAYNAME]
+		FROM V_Takeda_VACL_Contract_ReadPrivilege s
+		WHERE s.objectid =p1.objectid
+		FOR XML PATH('')),1,1,''),'&amp;','&') 
+	AS ACL_AllPermissions_GroupAndUserList
+
+		, (SELECT COUNT(DISTINCT ACLID) as CountACLID
+		FROM V_Takeda_VACL_Contract_ReadPrivilege s
+		WHERE s.objectid =p1.objectid
+		and USERID is not null
+		)
+	AS ACL_UserPermissions_Count
+
+		, (SELECT COUNT(DISTINCT ACLID) as CountACLID
+		FROM V_Takeda_VACL_Contract_ReadPrivilege s
+		WHERE s.objectid =p1.objectid
+		and USERGROUPID is not null
+		)
+	AS ACL_GroupPermissions_Count
+
+		, (SELECT COUNT(DISTINCT ACLID) as CountACLID
+		FROM V_Takeda_VACL_Contract_ReadPrivilege s
+		WHERE s.objectid =p1.objectid			
+		)
+	AS ACL_GroupAndUserPermissions_Count
+
+  FROM V_Takeda_VACL_Contract_ReadPrivilege p1
+  /*  WHERE 
+	OBJECTID = 148186 /* contractnumber = 'TEST-00000080' */ */
+	/* AND OBJECTTYPEID = 1 *//* already filtered in the view, contract only, since LINC does not have separate document permissions */
+  group by OBJECTID
+
+GO
