629a9c1ac9627ed6bfc9456e91594a15df3244c5
Susanne J <74077559+susannejoest@users.noreply.github.com>
2024-08-01 21:18:49 +0200

    Update Operators Any All etc.sql

--- Operators Any All etc.sql
+++ Operators Any All etc.sql
@@ -13,7 +13,7 @@ select studentname, marks
 from examresult
 where marks >= ALL (select marks from examresult)
 
-IF 3 < SOME (SELECT ID FROM T1)  
+IF 3 < SOME (SELECT ID FROM T1)  /* There is no functional or performance benefit in choosing SOME over ANY or vice versa */
 PRINT 'TRUE'   
 ELSE  
 PRINT 'FALSE' ;
